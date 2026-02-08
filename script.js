e55a9e8ca10df77fa299ff905d991ab549e70cc4
R Sawant <sawantraghunath0@gmail.com>
2026-02-08 15:42:23 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll("a").forEach(link => {
-  link.addEventListener("click", e => {
-    const target = document.querySelector(link.getAttribute("href"))
-    if(target){
-      e.preventDefault()
-      target.scrollIntoView({ behavior: "smooth" })
-    }
-  })
-})
